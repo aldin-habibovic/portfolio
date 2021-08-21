e86ea6deec3aaa606a3e6a1e30570c42e8abb3cb
Habibovic <Habibovic@LP35217>
2021-08-21 23:29:14 +0200

    commiting project structure

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Footer from './footer/Footer';
+import Header from './header/Header/Header';
+import About from './pages/about/About';
+import Education from './pages/education/Education';
+import Profile from './pages/profile/Profile';
+import Skills from './pages/skills/Skills';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <div className="page-content">
+        <div>
+          <Profile></Profile>
+          <About></About>
+          <Skills></Skills>
+          <Education></Education>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/about/About.js
@@ -0,0 +1,46 @@
+const About = () => {
+    return (
+        <div className="section" id="about">
+            <div className="container">
+                <div className="card" data-aos="fade-up" data-aos-offset="10">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12">
+                            <div className="card-body">
+                                <div className="h4 mt-0 title">About</div>
+                                <p>Hello! I am Aldin Habibović. Software Engineer</p>
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." <a href="https://templateflip.com/templates/creative-cv/" target="_blank">Learn More</a></p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="card-body">
+                                <div className="h4 mt-0 title">Basic Information</div>
+                                <div className="row">
+                                    <div className="col-sm-4"><strong className="text-uppercase">Age:</strong></div>
+                                    <div className="col-sm-8">30</div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-sm-4"><strong className="text-uppercase">Email:</strong></div>
+                                    <div className="col-sm-8">todo</div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-sm-4"><strong className="text-uppercase">Phone:</strong></div>
+                                    <div className="col-sm-8">todo</div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-sm-4"><strong className="text-uppercase">Address:</strong></div>
+                                    <div className="col-sm-8">todo</div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-sm-4"><strong className="text-uppercase">Language:</strong></div>
+                                    <div className="col-sm-8">English, German</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
