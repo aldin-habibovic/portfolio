9a9682feafc0ac437049c6cf6cdb2a98b8e06895
Habibovic <Habibovic@LP35217>
2021-08-27 17:11:52 +0200

    structuring components

---
+++ src/pages/education/Education-Item.tsx
@@ -0,0 +1,32 @@
+export interface IEducationItem {
+    start: Date;
+    end?: Date;
+    level: string;
+    university: string;
+    degreeCourse: string;
+    description: string;
+}
+
+const EducationItem = (props: { educationItem: IEducationItem }) => {
+    const { level, university, degreeCourse, description } = props.educationItem;
+
+    return (
+        <div className="row">
+            <div className="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+                <div className="card-body cc-education-header">
+                    <p>2007 - 2009</p>
+                    <div className="h5">{level}</div>
+                </div>
+            </div>
+            <div className="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+                <div className="card-body">
+                    <div className="h5">{degreeCourse}</div>
+                    <p className="category">{university}</p>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EducationItem;
---
+++ src/pages/skills/Skill-Item.tsx
@@ -0,0 +1,21 @@
+export interface ISkillItem {
+    name: string;
+    progress: string;
+}
+
+const SkillItem = (props: { skill: ISkillItem }) => {
+    const { name, progress } = props.skill;
+    return (
+        <div className="col-md-6">
+            <div className="progress-container progress-primary"><span className="progress-badge">{name}</span>
+                <div className="progress">
+                    <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar"
+                        aria-valuenow={60} aria-valuemin={0} aria-valuemax={100} style={{ width: progress }}></div>
+                    <span className="progress-value">{progress}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SkillItem;
