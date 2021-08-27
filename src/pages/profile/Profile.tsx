37fdb0e30827614448827fa25f3a75429a39e1e3
Habibovic <Habibovic@LP35217>
2021-08-27 17:16:16 +0200

    changing image url

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -3,7 +3,7 @@ const Profile = () => {
         <div className="profile-page">
             <div className="wrapper">
                 <div className="page-header page-header-small" filter-color="green">
-                    <div className="page-header-image" data-parallax="true" style={{ backgroundImage: "url('images/cc-bg-1.jpg')" }}></div>
+                    <div className="page-header-image" data-parallax="true" style={{ backgroundImage: "url('images/panorama-mostar-bg.jpg')" }}></div>
                     <div className="container">
                         <div className="content-center">
                             <div className="cc-profile-image"><a href="/#"><img src="images/me.jpg" alt="" /></a></div>
