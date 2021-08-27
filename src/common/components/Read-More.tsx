758290b244eb980176039acdfb270ed6506077ad
Habibovic <Habibovic@LP35217>
2021-08-27 17:12:29 +0200

    added read-more componenet

---
+++ src/common/components/Read-More.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const DEFAULT_LENGTH = 450;
+
+export interface IReadMore {
+    length?: number;
+    content: string;
+}
+
+const ReadMore = (props: IReadMore) => {
+    const { content, length } = props;
+    const [isReadMore, setIsReadMore] = useState(true);
+
+    const toggleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    };
+
+    const collapseText = (text: string) => {
+        return text.slice(0, length || DEFAULT_LENGTH);
+    }
+
+    return (
+        <p>
+            {isReadMore ? collapseText(content) : content}
+            <a onClick={toggleReadMore} href="/#showMore"> {isReadMore ? "...Read more" : " Show Less"} </a>
+        </p>
+    );
+}
+
+export default ReadMore;
