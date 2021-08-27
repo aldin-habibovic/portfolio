cdf2764d7899e1cd39da16a12355fb514c972c51
Habibovic <Habibovic@LP35217>
2021-08-27 17:17:20 +0200

    app modulized

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import './App.css';
 import Footer from './footer/Footer';
 import Header from './header/Header';
-import About from './pages/about/About';
+import About, { IAboutProps } from './pages/about/About';
+import Contact from './pages/contact/Contact';
 import Education from './pages/education/Education';
+import Portfolio from './pages/portfolio/Portfolio';
 import Profile from './pages/profile/Profile';
+import Reference from './pages/reference/Reference';
 import Skills from './pages/skills/Skills';
 
 function App() {
+  const about: IAboutProps = {aboutMe: '', age: 30, email: '', phone: '', adress: '', language: []};
+
   return (
     <>
       <Header></Header>
       <div className="page-content">
         <div>
           <Profile></Profile>
-          <About></About>
+          <About about={about} />
           <Skills></Skills>
           <Education></Education>
+          <Reference></Reference>
+          <Contact></Contact>
+          <Portfolio></Portfolio>
         </div>
       </div>
       <Footer></Footer>
