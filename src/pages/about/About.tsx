c1e2606bd4219e45d1c0596f833f5c93fe76b990
Habibovic <Habibovic@LP35217>
2021-08-27 17:14:14 +0200

    defining props for component

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import ReadMore from '../../common/components/Read-More';
 
-const About = () => {
+export interface IAboutProps {
+    aboutMe: string;
+    age: number;
+    email: string;
+    phone: string;
+    adress: string;
+    language: string[];
+}
+
+const About = (props: { about: IAboutProps }) => {
+    const { aboutMe, age, email, phone, adress, language } = props.about;
+
+    const desc = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.....Aysen ich liebe dich soooooooooo sehr ob du es glaubst oder nicht duuuu dummmmy :D"';
+
     return (
         <div className="section" id="about">
             <div className="container">
@@ -10,7 +24,7 @@ const About = () => {
                             <div className="card-body">
                                 <div className="h4 mt-0 title">About</div>
                                 <p>Hello! I am Aldin Habibović. Software Engineer</p>
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." <a href="https://templateflip.com/templates/creative-cv/" target="_blank" rel="noreferrer">Learn More</a></p>
+                                <ReadMore content={desc}></ReadMore>
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-12">
@@ -18,19 +32,19 @@ const About = () => {
                                 <div className="h4 mt-0 title">Basic Information</div>
                                 <div className="row">
                                     <div className="col-sm-4"><strong className="text-uppercase">Age:</strong></div>
-                                    <div className="col-sm-8">30</div>
+                                    <div className="col-sm-8">{age}</div>
                                 </div>
                                 <div className="row mt-3">
                                     <div className="col-sm-4"><strong className="text-uppercase">Email:</strong></div>
-                                    <div className="col-sm-8">todo</div>
+                                    <div className="col-sm-8">{email}</div>
                                 </div>
                                 <div className="row mt-3">
                                     <div className="col-sm-4"><strong className="text-uppercase">Phone:</strong></div>
-                                    <div className="col-sm-8">todo</div>
+                                    <div className="col-sm-8">{phone}</div>
                                 </div>
                                 <div className="row mt-3">
                                     <div className="col-sm-4"><strong className="text-uppercase">Address:</strong></div>
-                                    <div className="col-sm-8">todo</div>
+                                    <div className="col-sm-8">{adress}</div>
                                 </div>
                                 <div className="row mt-3">
                                     <div className="col-sm-4"><strong className="text-uppercase">Language:</strong></div>
