a4a9054572a7568ff949429038a2d9ca1d2d2abd
Habibovic <Habibovic@LP35217>
2021-09-01 18:29:25 +0200

    inital code for sending email

--- portfolio-email-sender/functions/src/index.ts
+++ portfolio-email-sender/functions/src/index.ts
@@ -1,9 +1,31 @@
 import * as functions from "firebase-functions";
 
+import { createTransport } from "nodemailer";
+
+
+const mailTransport = createTransport({
+  service: 'gmail',
+  auth: {}
+});
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
+
+  const mailOptions = {
+    from: "test@gmail.com",
+    replyTo: "test@gmail.com",
+    to: "",
+    subject: "test email",
+    text: "text text"
+  }
+
+  mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New email sent to: ' );
+    response.status(200).send({isEmailSend: true});
+  });
+
   response.send("Hello from Firebase!");
 });
