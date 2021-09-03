4dc626ad4bb0f1e102cbb9ac04e1c81f25ed3817
Habibovic <Habibovic@LP35217>
2021-09-03 17:17:11 +0200

    defined server urls for swagger & defining consumes&produces

--- portfolio-backend/src/main/java/com/me/portfolio/boundary/PortfolioController.java
+++ portfolio-backend/src/main/java/com/me/portfolio/boundary/PortfolioController.java
@@ -1,21 +1,24 @@
 package com.me.portfolio.boundary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.me.portfolio.model.Profile;
 import com.me.portfolio.repository.ProfileRepository;
 
 @RestController
+@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class PortfolioController {
 
     @Autowired
     private ProfileRepository profileRepository;
 
-    @GetMapping(value = "/read", consumes = "application/json", produces = "application/json")
+    @GetMapping(value = "/read", consumes = { MediaType.ALL_VALUE })
     public Profile index() {
         return profileRepository.findByFirstName("Aldin");
     }
