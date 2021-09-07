af0771fac8356c04b64ecdb8ce9275e3e61fedf3
Habibovic <Habibovic@LP35217>
2021-09-07 13:19:09 +0200

    adding cors configuration

--- portfolio-backend/src/main/java/com/me/portfolio/AccessingDataMongodbApplication.java
+++ portfolio-backend/src/main/java/com/me/portfolio/AccessingDataMongodbApplication.java
@@ -3,6 +3,9 @@ package com.me.portfolio;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -20,6 +23,16 @@ public class AccessingDataMongodbApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfgiurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/read").allowedOrigins("https://aldinhabibovic.me");
+            }
+        };
     }
 
 }
