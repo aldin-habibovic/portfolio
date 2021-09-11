0d04644bcdb874db4f7e563e7c1f15b579e8a0ca
Habibovic <Habibovic@LP35217>
2021-09-11 13:01:50 +0200

    contact from send email initial commit

---
+++ portfolio-frontend/src/pages/contact/Conact-Form.tsx
@@ -0,0 +1,99 @@
+import React from "react"
+
+class ContactForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            subject: '',
+            email: '',
+            message: '',
+            date: new Date().toLocaleString()
+        }
+    }
+
+    onNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ name: event.target.value })
+    }
+
+    onSubjectChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ name: event.target.value })
+    }
+
+    onEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ email: event.target.value })
+    }
+
+    onMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({ message: event.target.value })
+    }
+
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        fetch('https://us-central1-my-portfolio-323610.cloudfunctions.net/emails', {
+            method: "POST",
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then(
+            (response) => (response.json())
+        ).then((response) => {
+            if (response.status === 'success') {
+                alert("Message Sent.");
+            } else if (response.status === 'fail') {
+                alert("Message failed to send.")
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="col-md-6" >
+                <div className="card-body">
+                    <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+                        <div className="p pb-3"><strong>Feel free to contact me </strong></div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <div className="input-group"><span className="input-group-addon"><i className="fa fa-user-circle"></i></span>
+                                    <input className="form-control" type="text" name="name" onChange={this.onNameChange.bind(this)} placeholder="Name" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <div className="input-group"><span className="input-group-addon"><i className="fa fa-file-text"></i></span>
+                                    <input className="form-control" type="text" name="Subject" onChange={this.onSubjectChange.bind(this)} placeholder="Subject" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <div className="input-group"><span className="input-group-addon"><i className="fa fa-envelope"></i></span>
+                                    <input className="form-control" type="email" name="_replyto" onChange={this.onEmailChange.bind(this)} placeholder="E-mail" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <div className="form-group">
+                                    <textarea className="form-control" name="message" onChange={this.onMessageChange.bind(this)} placeholder="Your Message" required></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <button className="btn btn-primary" type="submit">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ContactForm;
--- portfolio-frontend/src/pages/contact/Contact-Section.tsx
+++ portfolio-frontend/src/pages/contact/Contact-Section.tsx
@@ -1,4 +1,5 @@
 import { BasicInformationDTO } from "../../../FETCHERS";
+import ContactForm from "./Conact-Form";
 
 const ContactSection = (props: { basicInfos: BasicInformationDTO }) => {
 
@@ -14,46 +15,7 @@ const ContactSection = (props: { basicInfos: BasicInformationDTO }) => {
                                 <div className="card mb-0" data-aos="zoom-in">
                                     <div className="h4 text-center title">Contact Me</div>
                                     <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="card-body">
-                                                <form action="https://formspree.io/your@email.com" method="POST">
-                                                    <div className="p pb-3"><strong>Feel free to contact me </strong></div>
-                                                    <div className="row mb-3">
-                                                        <div className="col">
-                                                            <div className="input-group"><span className="input-group-addon"><i className="fa fa-user-circle"></i></span>
-                                                                <input className="form-control" type="text" name="name" placeholder="Name" required/>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="row mb-3">
-                                                        <div className="col">
-                                                            <div className="input-group"><span className="input-group-addon"><i className="fa fa-file-text"></i></span>
-                                                                <input className="form-control" type="text" name="Subject" placeholder="Subject" required/>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="row mb-3">
-                                                        <div className="col">
-                                                            <div className="input-group"><span className="input-group-addon"><i className="fa fa-envelope"></i></span>
-                                                                <input className="form-control" type="email" name="_replyto" placeholder="E-mail" required/>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="row mb-3">
-                                                        <div className="col">
-                                                            <div className="form-group">
-                                                                <textarea className="form-control" name="message" placeholder="Your Message" required></textarea>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div className="row">
-                                                        <div className="col">
-                                                            <button className="btn btn-primary" type="submit">Send</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
+                                        <ContactForm></ContactForm>
                                         <div className="col-md-6">
                                             <div className="card-body">
                                                 <p className="mb-0"><strong>First name </strong></p>
