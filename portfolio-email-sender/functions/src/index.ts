b7f0351a95cd7ad7e207910c806c4fca5c73382f
Habibovic <Habibovic@LP35217>
2021-09-11 11:38:58 +0200

    firebase function

--- portfolio-email-sender/functions/src/index.ts
+++ portfolio-email-sender/functions/src/index.ts
@@ -1,36 +1,13 @@
 import * as functions from "firebase-functions";
 
-import {createTransport} from "nodemailer";
-
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-
-const mailTransport = createTransport({
-  service: "gmail",
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
 
-  const mailOptions = {
-    from: "test@gmail.com",
-    replyTo: "test@gmail.com",
-    to: gmailEmail,
-    subject: "test email",
-    text: "text text",
-  };
 
-  mailTransport.sendMail(mailOptions).then(() => {
-    console.log("New email sent to: " + gmailEmail);
-    response.status(200).send({isEmailSend: true});
-  });
 
-  response.send("Hello from Firebase!");
+  res.send("Hello from Firebase!");
 });
