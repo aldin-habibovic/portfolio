a304f29eb1f05b562148eb93ba34a4f13a34b4f1
Habibovic <Habibovic@LP35217>
2021-09-11 13:09:23 +0200

    fix build issue

--- portfolio-frontend/src/pages/contact/Conact-Form.tsx
+++ portfolio-frontend/src/pages/contact/Conact-Form.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 class ContactForm extends React.Component {
 
-    constructor(props) {
+    constructor(props: any) {
         super(props);
         this.state = {
             name: '',
